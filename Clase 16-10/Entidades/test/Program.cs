c468c212f52fc91b95795b53c5b44abe21902a2c
alumno <alumno@utnfra.com.ar>
2019-10-16 11:58:49 -0300

    asdasd

---
+++ Clase 16-10/Entidades/test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _16_10;
+
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recibo r1 = new Recibo();
+            Recibo r2 = new Recibo(3);
+            Factura f1 = new Factura(2);
+            Documento d1 = new Documento(4);
+
+            Contabilidad<Factura, Recibo> c1 = new Contabilidad<Factura, Recibo>();
+            c1 = c1 + f1;
+            Console.WriteLine("Se agrego");
+            c1 = c1 + r1;
+            c1 = c1 + r2;
+
+            Console.ReadLine();
+        }
+    }
+}
